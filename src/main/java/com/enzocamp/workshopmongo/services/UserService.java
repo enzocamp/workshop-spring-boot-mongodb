a7c7252c351e387178b376376d6b0871d1479ee0
enzocamp <enzo.flap@hotmail.com>
2025-09-09 23:22:56 -0300

    Get USer by id, and objectnotfound error handling with exception handler

--- src/main/java/com/enzocamp/workshopmongo/services/UserService.java
+++ src/main/java/com/enzocamp/workshopmongo/services/UserService.java
@@ -1,19 +1,27 @@
 package com.enzocamp.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enzocamp.workshopmongo.domain.User;
 import com.enzocamp.workshopmongo.repository.UserRepository;
+import com.enzocamp.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
+	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
